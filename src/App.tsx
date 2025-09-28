73d9ae975a78a410d5ccdb317e4d492a2a51b462
vlopez2828 <vickylopez2828@gmail.com>
2025-09-28 20:23:36 -0300

    feat(header): add header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import Header from "./components/Header"
 
 function App() {
 
   return (
-    <>
-      <h1>Hola</h1>
-    </>
+    <main className="p-4 min-h-screen bg-gradient-to-br from-black via-slate-950 to-blue-950">
+      <Header/>
+    </main>
   )
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,9 @@
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <section className="rounded-xl px-6 py-4 bg-white/5 backdrop-blur-md backdrop-saturate-150 border border-white/15 shadow-[0_10px_30px_-10px_rgba(0,0,0,0.6)]">
+        <h1 className="text-white text-4xl text-center">Mis Tareas</h1>
+    </section>
   )
 }
 
