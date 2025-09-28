786582865f824c141fdd5487da05738348fbd688
vlopez2828 <vickylopez2828@gmail.com>
2025-09-28 20:49:26 -0300

    style(header):change styles header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 
 const Header = () => {
   return (
-    <section className="rounded-xl px-6 py-4 bg-white/5 backdrop-blur-md backdrop-saturate-150 border border-white/15 shadow-[0_10px_30px_-10px_rgba(0,0,0,0.6)]">
+    <section className="px-6 py-4 bg-black-950  shadow-[0_8px_20px_-5px_rgba(59,130,246,0.6)]">
         <h1 className="text-white text-4xl text-center">Mis Tareas</h1>
     </section>
   )
