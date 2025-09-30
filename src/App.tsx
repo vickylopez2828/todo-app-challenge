105bdbc5b802021813b7673b8a4ddba3e6a59824
vlopez2828 <vickylopez2828@gmail.com>
2025-09-30 19:48:47 -0300

    feat(app): integrate TaskForm, TaskList, Footer and useTasks hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import Header from "./components/Header"
+import TaskForm from "./components/TaskForm"
+import TaskList from './components/TaskList';
+import Footer from "./components/Footer";
+import { useTasks } from "./hooks/useTasks";
 
 function App() {
+  const {tasks, addTask, deleteTask, completeTask} = useTasks();
 
   return (
-    <main className="p-4 min-h-screen bg-gradient-to-br from-black via-slate-950 to-blue-950">
+    <main className="min-h-dvh flex flex-col text-white bg-gradient-to-br from-black via-slate-950 to-blue-950">
       <Header/>
+      <section className="px-4 flex-1">
+        <TaskForm 
+          onAddTask={addTask}
+        />
+        <TaskList tasks={tasks} onDelete={deleteTask} onComplete={completeTask}/>
+      </section>
+      <div className="mt-auto">
+        <Footer/>
+      </div>
     </main>
   )
 }
