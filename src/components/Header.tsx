8771302be7656e7d7bb2636428ef016d6fe2c202
vlopez2828 <vickylopez2828@gmail.com>
2025-09-30 19:46:32 -0300

    style(header): update header styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,8 @@
 
 const Header = () => {
   return (
-    <section className="px-6 py-4 bg-black-950  shadow-[0_8px_20px_-5px_rgba(59,130,246,0.6)]">
-        <h1 className="text-white text-4xl text-center">Mis Tareas</h1>
+    <section className="p-6 bg-black shadow-[0_8px_20px_-5px_rgba(59,130,246,0.6)]">
+        <h1 className="text-white text-5xl text-center">Mis Tareas</h1>
     </section>
   )
 }
